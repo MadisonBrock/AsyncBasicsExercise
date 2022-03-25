b87e4ff653c06186a8c72e3e79b64489df3daa2c
Madison <madison.brock03@gmail.com>
2022-03-25 14:40:06 -0500

    Completed Exercise

---
+++ app.js
@@ -0,0 +1,70 @@
+// ASYNC BASICS EXERCISE
+// 1
+let name1 = "Luke Skywalker";
+
+function lukeName() {
+  console.log(`Question 1`);
+  console.log(name1);
+}
+
+function luke(n, callback) {
+  setTimeout(() => {
+    name1 = n;
+  }, 1000);
+}
+luke(`Mark Hamill`, n =>{
+    lukeName(name1);
+});
+
+
+
+// 2
+let name2 = "Han Solo";
+
+function hanName() {
+  console.log(`Question 2`);
+  console.log(name2);
+}
+
+function han(n) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      name2 = n;
+      resolve();
+    }, 2000);
+  });
+}
+
+hanName("Han Solo")
+.then(`Harrison Ford`)
+
+
+
+
+// 3
+let name3 = "Leia Organa";
+
+function leiaName() {
+  console.log(`Question 3`);
+  console.log(name3);
+}
+
+function leia(n) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      name3 = n;
+      resolve();
+    }, 3000);
+  });
+}
+async function example1(){
+    await leiaName(`Carrie Fisher`)
+    Carrie()
+}
+
+function Carrie(){
+    console.log(`Example 1`);
+    console.log(name3);
+  }
+
+  example1()
